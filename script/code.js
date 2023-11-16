632f81f090944f9da592009abedcca9f5e0372b5
fortuinp <pinda.fortuin@gmail.com>
2023-11-16 08:43:51 +0200

    update

--- script/code.js
+++ script/code.js
@@ -1,11 +1,9 @@
 let numbers=[12, 18, 11, 5, 8]
 
-// console.log("yoow "+ numbers.length);
-//console.log("shit " + numbers[(numbers.length)/2-1], numbers[numbers.length/2]);
 
 function challenge(){
     
-    // for (i=0; i<=numbers.length; i++){
+
         if(numbers.length%2==0 ){
             console.log( numbers[(numbers.length)/2-1], numbers[numbers.length/2]);
             document.getElementById('output').textContent = 
@@ -17,7 +15,7 @@ function challenge(){
             document.getElementById('output').textContent = numbers[parseInt(numbers.length/2)];
         }
     
-    // }
+
     
 }
 
